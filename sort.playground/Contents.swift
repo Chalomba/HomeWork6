35dfef6861748e844e13774332da3d138227d26e
chalomba <kirillcholombitko@gmail.com>
2016-06-09 18:37:04 +0300

    sort test

---
+++ sort.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Driver{
+    var name: String
+    var age: Int
+    
+    init (driverName: String, driverAge: Int){
+        name = driverName
+        age = driverAge
+    }
+}
+let driver1 = Driver (driverName: "Misha", driverAge: 25)
+let driver2 = Driver (driverName: "Stepa", driverAge: 24)
+let driver3 = Driver (driverName: "Petya", driverAge: 28)
+let driver4 = Driver (driverName: "Sasha", driverAge: 22)
+let driver5 = Driver (driverName: "Vitya", driverAge: 21)
+
+class Car{
+    var
+}
